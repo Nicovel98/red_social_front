074fb34fe9f7de2f1d49b948ac6579f2345c4d00
Nicovel <nicovel98@gmail.com>
2024-09-05 06:51:30 -0500

    global fix

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,6 +1,6 @@
 //Ruta del Backend desplegado
-export const Global = () => {
-    url: "https://red-social-back-23c2.onrender.com/api/";
+export const Global = {
+    url: "https://red-social-back-23c2.onrender.com/api/"
 }
 
 // Ruta del Backend en local
